c247ed2c6bb1673cbe874f45a6eed789f6f3e47b
Marco <102701262+MSCoding32@users.noreply.github.com>
2022-05-27 12:46:38 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+--[[
+███    ███ ███████      
+████  ████ ██        
+██ ████ ██ ███████    
+██  ██  ██      ██  
+██      ██ ███████                                                                               
+]]
+
+fx_version 'adamant'
+game 'gta5'
+
+author "Fallout to Resource by MS"
+
+description "Lua mod menu"
+
+client_script {
+    'falloutmenu.lua',
+    'dopamine_v2.lua'
+}
+
